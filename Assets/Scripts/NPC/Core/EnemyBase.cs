69be3a1587157ba4d9f5e3ca73eb0e1cd005a5a9
Dalbong0629 <20211398@vision.hoseo.edu>
2025-12-03 05:59:46 +0900

    12.3 1차 수정
    SwordHuman 제작

--- Assets/Scripts/NPC/Core/EnemyBase.cs
+++ Assets/Scripts/NPC/Core/EnemyBase.cs
@@ -12,22 +12,15 @@ namespace Project.NPC
         [Header("Ranges")] public float detectRange = 8f; public float loseRange = 12f; public float meleeRange = 1.1f;
         [Header("Combat")] public float attackDelay = 1.6f; public int contactDamage = 1; public LayerMask playerLayers;
         [Header("Refs")] public Animator anim; public Rigidbody2D rb; public Transform gfxRoot; public Transform attackOrigin; public AnimParams animParams;
-        [Header("Visual")]
-        public bool useSpriteFlip = true;   // ← On 권장
-        private SpriteRenderer _sr;
 
         protected int _hp; protected EnemyState _state; protected Transform _player; protected bool _facingRight = true; protected bool _busy = false; protected float _lastAttack = -999f;
         public bool IsDead => _state == EnemyState.Dead;
 
         protected virtual void Awake()
         {
-            if (!rb) rb = GetComponent(typeof(UnityEngine.Rigidbody2D)) as UnityEngine.Rigidbody2D;
-            if (!anim) anim = GetComponentInChildren(typeof(UnityEngine.Animator)) as UnityEngine.Animator;
-
-            if (gfxRoot)
-                _sr = gfxRoot.GetComponentInChildren<SpriteRenderer>(true);
+            if (!rb) rb = GetComponent<Rigidbody2D>();
+            if (!anim) anim = GetComponentInChildren<Animator>();
         }
-
 
         protected virtual void OnEnable()
         {
@@ -38,37 +31,8 @@ namespace Project.NPC
         {
             if (IsDead) return;
             AcquirePlayer();
-            // ※ 여기서는 '어느 쪽을 봐야 할지'만 계산하고, 실제 적용은 LateUpdate에서
-            ComputeFacing();
+            UpdateFacing();
             StateUpdate();
-        }
-        protected virtual void LateUpdate()
-        {
-            // 애니메이터가 값을 적용한 뒤 최종적으로 시각 플립을 덮어쓴다
-            ApplyFacing();
-        }
-        private bool _wantRight = true;
-        protected void ComputeFacing()
-        {
-            if (!_player || !gfxRoot) return;
-            _wantRight = (_player.position.x >= transform.position.x);
-        }
-        protected void ApplyFacing()
-        {
-            if (!_player || !gfxRoot) return;
-
-            if (useSpriteFlip && _sr)
-            {
-                // SpriteRenderer.flipX로 확실히 반전 (오른쪽 볼 때 flipX=false)
-                _sr.flipX = !_wantRight;
-            }
-            else
-            {
-                // 폴백: 스케일 반전
-                var s = gfxRoot.localScale;
-                s.x = Mathf.Abs(s.x) * (_wantRight ? 1f : -1f);
-                gfxRoot.localScale = s;
-            }
         }
 
         protected virtual void FixedUpdate()
@@ -166,6 +130,5 @@ namespace Project.NPC
 
         protected abstract void TryMelee();
         protected abstract void TrySkill(float distToPlayer);
-
     }
 }
